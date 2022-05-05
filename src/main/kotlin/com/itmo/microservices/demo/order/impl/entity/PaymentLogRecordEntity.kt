c2fcbca2a5b46822ccb29262a0bc6bd251bcab0e
kataevrsc <kataevrsc@bitbucket.org>
2022-05-05 20:34:42 +0300

    fix paymentlogrecord and itemmap entities ids

--- src/main/kotlin/com/itmo/microservices/demo/order/impl/entity/PaymentLogRecordEntity.kt
+++ src/main/kotlin/com/itmo/microservices/demo/order/impl/entity/PaymentLogRecordEntity.kt
@@ -3,11 +3,14 @@ package com.itmo.microservices.demo.order.impl.entity
 import com.itmo.microservices.demo.order.common.PaymentStatus
 import java.util.*
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
 class PaymentLogRecordEntity {
     @Id
+    @GeneratedValue
+    var id: UUID? = null
     var timestamp: Long? = null
     var status: PaymentStatus? = null
     var amount: Int? = null
@@ -15,7 +18,8 @@ class PaymentLogRecordEntity {
 
     constructor()
 
-    constructor(timestamp: Long, status: PaymentStatus, amount: Int, transactionId: UUID) {
+    constructor(id: UUID, timestamp: Long, status: PaymentStatus, amount: Int, transactionId: UUID) {
+        this.id = id
         this.timestamp = timestamp
         this.status = status
         this.amount = amount
