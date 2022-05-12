ca06807311b6c48dde87be430ad6f417988f9784
Владислав <v.pyslar@nyamee.com>
2022-05-12 11:10:32 +0300

    predStage

--- src/main/kotlin/com/itmo/microservices/demo/order/impl/repository/OrderRepository.kt
+++ src/main/kotlin/com/itmo/microservices/demo/order/impl/repository/OrderRepository.kt
@@ -1,7 +1,10 @@
 package com.itmo.microservices.demo.order.impl.repository
 
+import com.itmo.microservices.demo.order.impl.entity.ItemMapEntity
 import com.itmo.microservices.demo.order.impl.entity.OrderEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface OrderRepository: JpaRepository<OrderEntity, UUID>
+
+interface ItemMapRepository: JpaRepository<ItemMapEntity, UUID>
--- src/main/kotlin/com/itmo/microservices/demo/order/impl/service/DefaultOrderService.kt
+++ src/main/kotlin/com/itmo/microservices/demo/order/impl/service/DefaultOrderService.kt
@@ -1,20 +1,25 @@
 package com.itmo.microservices.demo.order.impl.service
 
+import com.itmo.microservices.demo.common.exception.NotFoundException
 import com.itmo.microservices.demo.delivery.api.model.BookingDto
 import com.itmo.microservices.demo.order.impl.entity.BookingEntity
 import com.itmo.microservices.demo.order.api.model.OrderModel
 import com.itmo.microservices.demo.order.api.model.PaymentLogRecordDto
 import com.itmo.microservices.demo.order.api.service.OrderService
+import com.itmo.microservices.demo.order.impl.entity.ItemMapEntity
 import com.itmo.microservices.demo.order.impl.entity.OrderEntity
 import com.itmo.microservices.demo.order.impl.entity.PaymentLogRecordEntity
+import com.itmo.microservices.demo.order.impl.repository.ItemMapRepository
 import com.itmo.microservices.demo.order.impl.repository.OrderRepository
+import org.springframework.data.crossstore.ChangeSetPersister
 import org.springframework.stereotype.Service
 import java.util.*
 import java.util.stream.Collectors
 
 @Service
 class DefaultOrderService (
-    val orderRepository: OrderRepository
+    val orderRepository: OrderRepository,
+    val itemMapRepository : ItemMapRepository,
         ) : OrderService {
     override fun createOrder(): OrderModel {
         TODO("Not yet implemented")
@@ -25,7 +30,22 @@ class DefaultOrderService (
     }
 
     override fun moveItemToCart(orderId: UUID, itemId: UUID, amount: Int) {
-        TODO("Not yet implemented")
+        val optionalOrder: Optional<OrderEntity> = orderRepository.findById(orderId);
+        val optionalItem: Optional<ItemMapEntity> = itemMapRepository.findById(itemId);
+        if (optionalOrder.isPresent && optionalItem.isPresent) {
+            val order: OrderEntity = optionalOrder.get();
+            val item: ItemMapEntity = optionalItem.get();
+
+            if (item.amount!! > 0) {
+                //order.itemsMap.add(item)
+                orderRepository.save(order);
+                eventBus
+            } else
+                throw NotFoundException("Amount item ($itemId) is 0");
+
+            //order.itemsMap.
+        } else
+            throw NotFoundException("Order or Item not found");
     }
 
     override fun finalizeOrder(id: UUID): BookingDto {
